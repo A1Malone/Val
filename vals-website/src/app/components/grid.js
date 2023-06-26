ccc55165b846d767e5823e6df5858ce57cfe7241
Alfred <deshawn.malone14@gmail.com>
2023-06-26 16:38:26 -0500

    import grid

---
+++ vals-website/src/app/components/grid.js
@@ -0,0 +1,9 @@
+export default function Grid() {
+    return (
+        <>
+            <div className="container">
+                <div className="row">a grid should be here</div>
+            </div>
+        </>
+    );
+};
--- vals-website/src/app/page.js
+++ vals-website/src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 
 import Example from "./components/nav";
+import Grid from "./components/grid";
 
 export default function Home() {
   return (
     <>
       <Example />
+      <Grid />
       <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
     </>
   );
