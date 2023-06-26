bc62a1091577319a60ef3514fb906de53f67dc0a
Alfred <deshawn.malone14@gmail.com>
2023-06-26 10:14:26 -0500

    add nav

--- vals-website/src/app/page.js
+++ vals-website/src/app/page.js
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 
+import Example from './components/nav'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
+<Example />
     </main>
   )
 }
