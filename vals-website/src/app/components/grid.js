f843dfbb409910d7bb667649195344c0a6f87003
Alfred <deshawn.malone14@gmail.com>
2023-06-28 21:19:05 -0500

    add grid

--- vals-website/src/app/components/grid.js
+++ vals-website/src/app/components/grid.js
@@ -1,9 +1,25 @@
 export default function Grid() {
-    return (
-        <>
-            <div className="container">
-                <div className="row">a grid should be here</div>
-            </div>
-        </>
-    );
-};
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="grid" data-masonry='{"percentPosition": true }'>
+            <div className="grid-item"></div>
+            <div className="grid-item grid-item--width2"></div>
+            <div className="grid-item grid-item--width2"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item grid-item--height2"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item grid-item--height2"></div>
+            <div className="grid-item grid-item--width2"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item grid-item--height2"></div>
+            <div className="grid-item grid-item--height2"></div>
+            <div className="grid-item grid-item--width2"></div>
+            ...
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
