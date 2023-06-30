1e8f6081acd15c08576e6531961fe9c833522eb9
Alfred <deshawn.malone14@gmail.com>
2023-06-29 21:35:46 -0500

    change image

--- vals-website/src/app/components/grid.js
+++ vals-website/src/app/components/grid.js
@@ -1,23 +1,12 @@
+import Image from "next/image";
 export default function Grid() {
   return (
     <>
-      <div >
-          <div className="gap-0 columns-3 columns-sm">
-  <img class="w-full aspect-video ..." src="..." />
-  <img class="w-full aspect-square ..." src="..." />
-  <img class="w-full aspect-video ..." src="..." />
-  <img class="w-full aspect-square ..." src="..." />
-  <img class="w-full aspect-video ..." src="..." />
-  <img class="w-full aspect-square ..." src="..." />
-  <img class="w-full aspect-video ..." src="..." />
-  <img class="w-full aspect-square ..." src="..." />
-  <img class="w-full aspect-video ..." src="..." />
-  <img class="w-full aspect-square ..." src="..." />
-  <img class="w-full aspect-video ..." src="..." />
-  <img class="w-full aspect-square ..." src="..." />
-  <img class="w-full aspect-video ..." src="..." />
-  <img class="w-full aspect-square ..." src="..." />
-          </div>
+      <div>
+        <div className="gap-0 columns-3 columns-sm">
+        <img class="w-full aspect-video ..." src="/assets/portfolio/1.jpg" />
+          
+        </div>
       </div>
     </>
   );
