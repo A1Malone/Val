e4daab4d0075d2b0f9324dee4e9ca029af15c4e2
Alfred <deshawn.malone14@gmail.com>
2023-06-30 20:40:05 -0500

    add grid image

--- vals-website/src/app/components/grid.js
+++ vals-website/src/app/components/grid.js
@@ -1,12 +1,14 @@
 export default function Grid() {
   return (
     <>
-      <div>
-        <div className="gap-0 columns-3 columns-sm">
-        <img class="w-full aspect-video ..." src="/assets/portfolio/1.jpg" />
-          
+        <div className="gap-0 columns-3">
+          <div className="imgContainer">
+            <img className="gridimg" src="/assets/portfolio/1.jpg" />
+            <div className="imgDescription">
+              <p className="gridText">hello child</p>
+            </div>
+          </div>
         </div>
-      </div>
     </>
   );
 }
