43ea6169ddb30416d82b7f7afa6e8d08fff167c1
Alfred <deshawn.malone14@gmail.com>
2023-07-01 14:15:26 -0500

    create component

---
+++ vals-website/src/app/components/projectView.js
@@ -0,0 +1,25 @@
+export default function Project(props) {
+  return (
+    <>
+      <div className="w-screen px-7">
+        <div class="flex gap-0 columns-3 columns-sm">
+          <div class="flex-none w-80 space-y-4 mt-10">
+            <p class="text-6xl leading-relaxed   ...">
+              The quick brown fox ...
+            </p>
+            <p class="text-xl ...">type of design</p>
+            <p class="text-xl pt-10">
+              more description stuff Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Ut tortor velit, fermentum vitae vestibulum et,
+              tincidunt quis mi.
+            </p>
+          </div>
+          <div class="grow px-2">
+            <img className="w-full" src={`/assets/portfolio/1.jpg`} />
+          </div>
+          <div class="flex-none w-auto">the next project </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- vals-website/src/app/portfolio/page/test/page.js
+++ vals-website/src/app/portfolio/page/test/page.js
@@ -1,25 +1,11 @@
 import Nav from "../../../components/nav";
+import Project from "../../../components/projectView";
+
 export default function PortfolioPage() {
   return (
     <>
       <Nav />
-      <div className="w-screen px-7">
-      <div class="flex gap-0 columns-3 columns-sm">
-        <div class="flex-none w-80 space-y-4 mt-10">
-          <p class="text-6xl leading-relaxed   ...">The quick brown fox ...</p>
-          <p class="text-xl ...">type of design</p>
-          <p class="text-xl pt-10">
-            more description stuff Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Ut tortor velit, fermentum vitae vestibulum et,
-            tincidunt quis mi.
-          </p>
-        </div>
-        <div class="grow px-2">
-            <img className="w-full" src={`/assets/portfolio/1.jpg`} /></div>
-        <div class="flex-none w-auto">the next project </div>
-      </div>
-
-      </div>
+      <Project />
     </>
   );
 }
