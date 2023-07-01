84eb08589b4d8621837047d488ba3299a2dcb6b3
Alfred <deshawn.malone14@gmail.com>
2023-07-01 12:39:49 -0500

    update page

---
+++ vals-website/src/app/portfolio/page/test/page.js
@@ -0,0 +1,17 @@
+export default function PortfolioPage() {
+  return (
+    <>
+    <div class="flex ...">
+  <div class="flex-none w-14 h-14 ...">
+    side bar
+  </div>
+  <div class="grow h-14 ...">
+    content goes here
+  </div>
+  <div class="flex-none w-14 h-14 ...">
+    the next feature
+  </div>
+</div>
+    </>
+  )
+}
