609fe187ed9727595b57c201bd06ae31b812ecb0
Alfred <deshawn.malone14@gmail.com>
2023-07-02 21:31:52 -0500

    update grid

--- vals-website/src/app/components/grid.js
+++ vals-website/src/app/components/grid.js
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 export default function Grid(props) {
   return (
     <>
+    <Link href={props.link}>
           <div className="imgContainer">
             <img className="gridimg" src={`/assets/portfolio/${props.img}`} />
             <div className="imgDescription">
               <p className="gridText">{props.name}</p>
             </div>
           </div>
+    </Link>
     </>
   );
 }
--- vals-website/src/app/page.js
+++ vals-website/src/app/page.js
@@ -1,70 +1,7 @@
-import Image from "next/image";
-
 import Nav from "./components/nav";
 import Grid from "./components/grid";
+import { projects } from "./constant";
 
-const Projects = [
-  {
-    img: "1.jpg",
-    name: "Val Bui",
-  },
-  {
-    img: "2.jpg",
-    name: "lOADING SCREENS",
-  },
-  {
-    img: "3.jpg",
-    name: "lOADING SCREENS IN ACTION",
-  },
-  {
-    img: "4.jpg",
-    name: "THE METHOD OF SMAPLING",
-  },
-  {
-    img: "5.jpg",
-    name: "SAMPLING IS A METHOD",
-  },
-  {
-    img: "6.jpg",
-    name: "WAVES",
-  },
-  {
-    img: "7.jpg",
-    name: "STEDMAN GALLERY THESIS SHOW",
-  },
-  {
-    img: "8.jpg",
-    name: "VMPA SENIOR THESIS SHOW",
-  },
-  {
-    img: "9.jpg",
-    name: "PHILLY FACES",
-  },
-  {
-    img: "10.jpg",
-    name: "LIVE LAUGH DIE",
-  },
-  {
-    img: "11.jpg",
-    name: "GHOSTED",
-  },
-  {
-    img: "12.jpg",
-    name: "THE SCIENCE OF SLEEP",
-  },
-  {
-    img: "13.jpg",
-    name: "CULTURE CONNECT",
-  },
-  {
-    img: "14.jpg",
-    name: "THE PREVIEW",
-  },
-  {
-    img: "15.jpg",
-    name: "IDK THE NAME BUT IT LOOKS COOL..... ITS A PREVIEW ANYWAYS... HAVE FUN! :)",
-  },
-]
 
 export default function Home() {
   return (
@@ -75,8 +12,8 @@ export default function Home() {
       <div className="gap-0 columns-3">
         
       {
-        Projects.map((nProject, i) => (
-          <Grid img={nProject.img} name={nProject.name} key={i} />
+        projects.map((nProject, i) => (
+          <Grid img={nProject.img} name={nProject.name} link={`/portfolio/page/${nProject.link}`} key={i} />
         ))
       }
       </div>
