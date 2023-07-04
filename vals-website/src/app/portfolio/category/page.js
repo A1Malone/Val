3bcb0ebf9da4c88a3cb8719c4d73a634074f3160
Alfred <deshawn.malone14@gmail.com>
2023-07-03 22:27:16 -0500

    make test template

--- vals-website/src/app/portfolio/category/page.js
+++ vals-website/src/app/portfolio/category/page.js
@@ -1,10 +1,16 @@
 import Nav from "../../components/nav";
+import Grid from "../../components/grid"
+import { projects } from "../../constant";
 
 export default function CategoryPage({params}) {
   return (
     <>
-      <Nav />
-      category Page
+    <Nav />
+    <div className="gap-0 columns-1 md:columns-3">
+    <Grid img={projects[2].img} name={projects[2].name} link={`/portfolio/page/${projects[2].link}`} />
+    <Grid img={projects[3].img} name={projects[3].name} link={`/portfolio/page/${projects[3].link}`} />
+    </div>
+    <p>*this is an example*</p>
     </>
   );
 }
