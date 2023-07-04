4ebb24b8ae88bb4671e1bef465a2440085f45131
Alfred <deshawn.malone14@gmail.com>
2023-07-03 20:48:37 -0500

    add category setup

---
+++ vals-website/src/app/portfolio/category/layout..js
@@ -0,0 +1,17 @@
+import '../../../../app/globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Val Bui - About',
+  description: 'Striving to create a brand identity through graphic and UX design that leaves a lasting impression for your audience.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
