f01b5834714bdc5bfe793ef92579a7712b58b9da
Alfred <deshawn.malone14@gmail.com>
2023-07-04 23:17:07 -0500

    add icons

--- vals-website/src/app/components/projectView.js
+++ vals-website/src/app/components/projectView.js
@@ -13,7 +13,15 @@ export default function Project(props) {
           <div class="grow px-2 ">
             <img className="w-full" src={`/assets/portfolio/${props.img}`} />
           </div>
-          <div class="flex-none w-auto mx-auto">the next project </div>
+          <div class="flex-none w-20 mx-auto">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9 stroke-2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9 stroke-2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+</svg>
+
+          </div>
         </div>
       </div>
     </>
