a1b0e581b099ebd275b499f406e4d42c26e5e598
Alfred <deshawn.malone14@gmail.com>
2023-07-04 22:46:47 -0500

    update constant

--- vals-website/src/app/constant.js
+++ vals-website/src/app/constant.js
@@ -2,76 +2,91 @@ export const projects = [
     {
       img: "1.jpg",
       name: "Val Bui",
-      link: "val-bui"
+      link: "val-bui",
+      description: "filler for now"
     },
     {
       img: "2.jpg",
       name: "lOADING SCREENS",
-      link: "loading-screens"
+      link: "loading-screens",
+      description: "The Art of Loading Screens is a website made to show the significance of loading screens of video games. With the use of vivid imagery and large scale text, the viewer is meant to learn how loading screens help both the user and the creators of the game."
     },
     {
       img: "3.jpg",
       name: "lOADING SCREENS IN ACTION",
-      link: "loading-screens-in-action"
+      link: "loading-screens-in-action",
+      description: "filler for now"
     },
     {
       img: "4.jpg",
       name: "THE METHOD OF SAMPLING",
-      link: "the-method-of-sampling"
+      link: "the-method-of-sampling",
+      description: "As part of the thesis show, The Method of Sampling analyzes how sampling in art is necessary and important to a creator. Allusions, detournement, and appropriation are all means of sampling which can improve an artist’s work."
     },
     {
       img: "5.jpg",
       name: "SAMPLING IS A METHOD",
-      link: "sampling-is-a-method"
+      link: "sampling-is-a-method",
+      description: "filler for now"
     },
     {
       img: "6.jpg",
       name: "WAVES",
-      link: "waves"
+      link: "waves",
+      description: "To many, music is a basic human need, on top of drinking water. The Waves brand captures the essence of a person who enjoys the outdoors and a listener of music."
     },
     {
       img: "7.jpg",
       name: "STEDMAN GALLERY THESIS SHOW",
-      link: "stedman-gallery-thesis-show"
+      link: "stedman-gallery-thesis-show",
+      description: "The Stedman Gallery thesis portrays works of the graduating seniors at Rutgers Camden. The show’s theme is pushing the boundaries and staying “Out of Line.” This work represents each senior’s project through shapes, as shapes are the foundation of all art. Art must push forward past the foundations and out of the box."
     },
     {
       img: "8.jpg",
       name: "VMPA SENIOR THESIS SHOW",
-      link: "vmpa-senior-thesis-show"
+      link: "vmpa-senior-thesis-show",
+      description: "filler for now"
     },
     {
       img: "9.jpg",
       name: "PHILLY FACES",
-      link: "philly-faces"
+      link: "philly-faces",
+      description: "Philly Faces is a rebrand of Philly Phaces. This non-profit organization helps children and families of those in need, particularly those with Congenital Facial Abnormalities."
     },
     {
       img: "10.jpg",
       name: "LIVE LAUGH DIE",
-      link: "live-laugh-die"
+      link: "live-laugh-die",
+      description: "This process book derives from a personal narrative love story called Live Laugh Die. With the use of text, scale, and imagery, this process book shows how a simple text can be transformed into various works."
     },
     {
       img: "11.jpg",
       name: "GHOSTED",
-      link: "ghosted"
+      link: "ghosted",
+      description: "filler for now"
     },
     {
       img: "12.jpg",
       name: "THE SCIENCE OF SLEEP",
-      link: "the-science-of-sleep"
+      link: "the-science-of-sleep",
+      description: "The Science of Sleep is a story of how a young man learns to cope with the death of his father. He becomes obsessed with his imagination and is imprisoned by his own mind. This redesign captures the image of his solitary mind and alternate reality."
     },
     {
       img: "13.jpg",
       name: "CULTURE CONNECT",
-      link: "culture-connect"
+      link: "culture-connect",
+      description: "Culture Connect is a brand with the purpose of helping immigrants adapt to America. Volunteers help immigrants with difficult tasks such as filing for taxes, filling out documents, and even learning how to understand English."
     },
     {
       img: "14.jpg",
-      name: "THE PREVIEW",
-      link: "the-preview"
+      name: "THE FOLD",
+      link: "the-fold",
+      description: "The Fold is a display typeface made to be expressive due to its thick strokes. Its unique shape makes it noticeable and helps attract the viewer’s eye."
     },
     {
       img: "15.jpg",
       name: "IDK THE NAME BUT IT LOOKS COOL..... ITS A PREVIEW ANYWAYS... HAVE FUN! :)",
-      link: "idk"
+      link: "idk",
+      description: "filler for now"
     },
   ];
