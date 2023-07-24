47fc5b9bd03f0e90957b17ee4a2003cb7ab84e3c
Alfred <deshawn.malone14@gmail.com>
2023-07-23 20:31:40 -0500

    fix for now

--- vals-website/src/app/about/page.js
+++ vals-website/src/app/about/page.js
@@ -7,14 +7,14 @@ export default function Home() {
     <>
       <Nav />
       <div className="text-center w-full lg:w-1/2 mx-auto pt-20">
-        <img className="w-60 mx-auto" src="/assets/_MG_0075.png" />
-        <p className="font-bold leading-loose text-4xl">Hi! I'm Val Bui.</p>
-        <p className="text-xl leading-loose px-2 mx-auto lg:w-[85%] lg:break-all lg:leading-loose lg:text-3xl">
+        <img className="w-60 pb-5 mx-auto" src="/assets/_MG_0075.png" />
+        <p className="font-bold leading-loose pb-3 text-4xl">Hi! I'm Val Bui.</p>
+        <p className="text-xl leading-loose break-all px-8 mx-auto lg:w-[85%] lg:break-all lg:leading-loose lg:text-3xl">
           Based in New Jersey, USA, I am an independent branding artist and
           photographer. With over 6 years of designing, I've been able to work
           with clients around the world.
         </p>
-        <div className="flex flex-col p-8 gap-4 lg:flex-row">
+        <div className="flex flex-col my-[15%] p-8 gap-4 lg:flex-row">
         <div className="flex items-center justify-center">
           <div className="flex flex-col">
           <a
