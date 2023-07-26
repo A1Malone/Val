14cccaf2f7d1639ad46024cd2f787e7b4e9f180f
AMV <deshawn.malone14@gmail.com>
2023-07-25 21:50:34 -0500

    component should work

--- vals-website/src/app/components/contact.js
+++ vals-website/src/app/components/contact.js
@@ -1,56 +1,38 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 export default function Contact(props) {
 
+
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ke1sdzj', 'template_wklo16p', form.current, 'DGv1nfXfttr9ZbCIk')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
     return (
         <>
-            <form action="#" method="POST" className="mx-auto max-w-xl">
+            <form ref={form} onSubmit={sendEmail} className="mx-auto max-w-xl">
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                    <div>
-                        <label
-                            htmlFor="first-name"
-                            className="block text-sm font-semibold leading-6 text-white"
-                        >
-                            First name
-                        </label>
-                        <div className="mt-2.5">
-                            <input
-                                type="text"
-                                name="first-name"
-                                id="first-name"
-                                autoComplete="given-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <label
-                            htmlFor="last-name"
-                            className="block text-sm font-semibold leading-6 text-white"
-                        >
-                            Last name
-                        </label>
-                        <div className="mt-2.5">
-                            <input
-                                type="text"
-                                name="last-name"
-                                id="last-name"
-                                autoComplete="family-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            />
-                        </div>
-                    </div>
                     <div className="sm:col-span-2">
                         <label
-                            htmlFor="subject"
+                            htmlFor="Name"
                             className="block text-sm font-semibold leading-6 text-white"
                         >
-                            Subject
+                            Name
                         </label>
                         <div className="mt-2.5">
                             <input
                                 type="text"
-                                name="subject"
-                                id="subject"
-                                autoComplete="organization"
+                                name="user_name"
+                                id="name"
+                                autoComplete="given-name"
                                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
@@ -65,7 +47,7 @@ export default function Contact(props) {
                         <div className="mt-2.5">
                             <input
                                 type="email"
-                                name="email"
+                                name="user_email"
                                 id="email"
                                 autoComplete="email"
                                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
