e3046310192f9b3efb29d849c1da3140f91394eb
AMV <deshawn.malone14@gmail.com>
2023-07-25 20:11:04 -0500

    create component

--- vals-website/src/app/about/page.js
+++ vals-website/src/app/about/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Nav from "../components/nav";
 import Footer from "../components/footer";
+import Contact from "../components/contact"
 
 export default function Home() {
   return (
@@ -34,103 +35,7 @@ export default function Home() {
         </div>
           <div className="w-full">
             <div className="isolate py-6">
-              <form action="#" method="POST" className="mx-auto max-w-xl">
-                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                  <div>
-                    <label
-                      htmlFor="first-name"
-                      className="block text-sm font-semibold leading-6 text-white"
-                    >
-                      First name
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="first-name"
-                        id="first-name"
-                        autoComplete="given-name"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="last-name"
-                      className="block text-sm font-semibold leading-6 text-white"
-                    >
-                      Last name
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="last-name"
-                        id="last-name"
-                        autoComplete="family-name"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                    <label
-                      htmlFor="subject"
-                      className="block text-sm font-semibold leading-6 text-white"
-                    >
-                      Subject
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="subject"
-                        id="subject"
-                        autoComplete="organization"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-semibold leading-6 text-white"
-                    >
-                      Email
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        autoComplete="email"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      />
-                    </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                    <label
-                      htmlFor="message"
-                      className="block text-sm font-semibold leading-6 text-white"
-                    >
-                      Message
-                    </label>
-                    <div className="mt-2.5">
-                      <textarea
-                        name="message"
-                        id="message"
-                        rows={4}
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        defaultValue={""}
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="mt-10">
-                  <button
-                    type="submit"
-                    className="block w-20 rounded-md bg-slate-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-slate-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </form>
+              <Contact />
             </div>
           </div>
         </div>
