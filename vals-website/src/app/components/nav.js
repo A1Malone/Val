257c9327e09e8b2087841f968c16bc32ca630375
Alfred <deshawn.malone14@gmail.com>
2024-02-09 19:38:59 -0600

    add link

--- vals-website/src/app/components/nav.js
+++ vals-website/src/app/components/nav.js
@@ -6,6 +6,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 const navigation = [
   { name: 'Home', href: '/', current: false },
   { name: 'About', href: '/about', current: false },
+  { name: 'Gallery', href: '/gallery', current: false },
 ]
 
 
