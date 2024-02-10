269f86fa3ae183af8142cd5b96871efda48eb7b0
Alfred <deshawn.malone14@gmail.com>
2024-02-09 19:36:15 -0600

    rmv

--- vals-website/src/app/gallery/page.js
+++ vals-website/src/app/gallery/page.js
@@ -40,7 +40,6 @@ export default function Home() {
           </div>
           <div className="w-full">
             <div className="isolate py-6">
-              <Contact />
             </div>
           </div>
         </div>
