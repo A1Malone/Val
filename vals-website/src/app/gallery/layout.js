c4b4e17e766cc0a336d8617669f9f8eb22b6d277
Alfred <deshawn.malone14@gmail.com>
2024-02-11 22:45:15 -0600

    change tag

--- vals-website/src/app/gallery/layout.js
+++ vals-website/src/app/gallery/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Val Bui - About',
+  title: 'Val Bui - Gallery',
   description: 'Striving to create a brand identity through graphic and UX design that leaves a lasting impression for your audience.',
 }
 
