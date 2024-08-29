4f6b200a9e84117ce62022e16a19d8c49cc24ecc
Alfred <deshawn.malone14@gmail.com>
2024-08-28 19:53:21 -0500

    update landing

--- vals-website/src/app/page.js
+++ vals-website/src/app/page.js
@@ -13,6 +13,15 @@ export default function Home() {
       <Nav/>
 
     </div>
+    <section className="h-screen">
+      <div className="flex items-center h-screen w-screen absolute">
+      <h1 className="text-7xl font-bold mx-auto lg:text-9xl">VAL BUI</h1>
+      </div>
+      <div className="flex items-end h-screen absolute"><p className="mb-10 ml-5 lg:mb-20 lg:ml-40">Graphic Design | Photography</p></div>
+    </section>
+    <section>
+
+    </section>
       {/* i removed bg-cover  */}
       <div className="min-h-screen bg-[length:100%_50%] bg-no-repeat bg-center md:bg-[length:100%_100%] lg:bg-contain" style={{backgroundImage: `url(${mainImg})`}}></div>
           
