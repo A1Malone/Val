77889a4528671a5acf78f39bb72a5ce16702fc62
Alfred <deshawn.malone14@gmail.com>
2025-01-19 18:46:54 -0600

    add book

--- vals-website/src/app/components/book.js
+++ vals-website/src/app/components/book.js
@@ -7,14 +7,16 @@ export default function Book(props) {
 
     return (
       <>
-            <div className="flex justify-center mb-5">
-          <HTMLFlipBook width={400}height={600} size="fixed" >
+            <div className="w-full mb-[96px]">
+          <HTMLFlipBook width={700} height={500} sixe="stretch" autoSize="true">
              {
                       pages.map((page, i) => (
-                        <div className="demoPage"> <img className="galleryImg" src={`/assets/portfolio/125-100 MFS PCD ANNIVERSARY/journal images/new/${page.img}`}  key={i}/> </div>   
+                        <div className="demoPage"> <img className="lg:w-full w-1/2 h-full" src={`/assets/portfolio/125-100 MFS PCD ANNIVERSARY/journal images/new/${page.img}`}  key={i}/> </div>   
                       ))
                     }
               </HTMLFlipBook>
+
+              <p className='mt-2'>please swipe to the next page.</p>
       
             </div>
       </>
